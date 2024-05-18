01db1c065064f57c3cb9cabc692f09b3f07c8a09
HoStWgSp <hostwg@vk.com>
2024-05-18 16:33:36 +1100

    Задание 13.5.8

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -8,49 +9,56 @@ namespace ArrayPractice
 {
     internal class Program
     {
-        public static Stack<string> words = new Stack<string>();
+        // объявим потокобезопасную очередь (полностью идентична обычной очереди, но
+        // позволяет безопасный доступ
+        // из разных потоков)
+        public static ConcurrentQueue<string> words = new ConcurrentQueue<string>();
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в стек.");
+            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в очередь.");
             Console.WriteLine();
+
+            //  запустим обработку очереди в отдельном потоке
+            StartQueueProcessing();
 
             while (true)
             {
-                string input = Console.ReadLine();
-
-
-                
-
-                switch (input)
+                var input = Console.ReadLine();
+                // если введена нужная нам команда - смотрим, кто крайний в очереди
+                if (input == "peek")
                 {
-                    //   если  команда pop - пробуем достать элемент
-                    case "pop":
-                        words.TryPop(out string popResult);
-                        Console.WriteLine("Удалено: {0}", popResult);
-                        break;
-                    //   если  команда peek - пробуем  просмотреть элемент
-                    case "peek":
-                        words.TryPeek(out string peekResult);
-                        Console.WriteLine("rgfewgdfgdgd {0}",peekResult);
-                        break;
-                    default:
-                        // если ни одна из команд не распознана - простов стек добавляем то что введено
-                        if (string.IsNullOrEmpty(input)) break;
-                        break;
+                    if (words.TryPeek(out var elem))
+                        Console.WriteLine(elem);
                 }
-                words.Push(input);
-                Console.WriteLine();
-                Console.WriteLine("В стеке:");
-
-                foreach (var word in words)
+                else
                 {
-                    Console.WriteLine(" " + word);
+                    // если не введена - ставим элемент в очередь, как и обычно
+                    words.Enqueue(input);
                 }
+
+                // words.Enqueue(input); // ИЗМЕНИТЬ ЗДЕСЬ
             }
 
             Console.ReadKey();
         }
-        
+        // метод, который обрабатывает и разбирает нашу очередь в отдельном потоке
+        // ( для выполнения задания изменять его не нужно )
+        static void StartQueueProcessing()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                while (true)
+                {
+                    Thread.Sleep(5000);
+                    if (words.TryDequeue(out var element))
+                        Console.WriteLine("======>  " + element + " прошел очередь");
+                }
+
+            }).Start();
+        }
     }
 }
