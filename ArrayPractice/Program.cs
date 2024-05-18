2bb397ec227381a0878c7d59ee79ace6d751b141
HoStWgSp <hostwg@vk.com>
2024-05-18 15:12:31 +1100

    Задание 13.3.12

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -9,36 +9,38 @@ namespace ArrayPractice
     {
         static void Main(string[] args)
         {
-            // Сохраняем предложение в строку
-            var sentence =
-                "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+            while (true)
+            {
+                Console.WriteLine("Введите строку");
+                // Сохраняем предложение в строку
+                var sentence = Console.ReadLine();
 
-            // сохраняем в массив char
-            var characters = sentence.ToCharArray();
+                // сохраняем в массив char
+                var characters = sentence.ToCharArray();
 
-            var symbols = new HashSet<char>();
+                var symbols = new HashSet<char>();
 
-            // добавляем во множество. Сохраняются только неповторяющиеся символы
-            foreach (var symbol in characters)
-                symbols.Add(symbol);
+                // добавляем во множество. Сохраняются только неповторяющиеся символы
+                foreach (var symbol in characters)
+                    symbols.Add(symbol);
 
-            // Выводим результат
-            Console.WriteLine(symbols.Count);
-            // сохраняем знаки препинания в массив Char
-            var signs = new[] { ',', ' ', '.' };
+                // Выводим результат
+                Console.WriteLine(symbols.Count);
+                // сохраняем знаки препинания в массив Char
+                var signs = new[] { ',', ' ', '.' };
 
-            // сохраняем числовые символы в массив Char
-            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                // сохраняем числовые символы в массив Char
+                var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-            //  Проверяем, есть ли цифры
-            bool containsNumbers = symbols.Overlaps(numbers);
-            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+                //  Проверяем, есть ли цифры
+                bool containsNumbers = symbols.Overlaps(numbers);
+                Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
 
-            // Отбрасываем знаки препинания и заново считаем
-            symbols.ExceptWith(signs);
-            Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
-
-            Console.ReadKey();
+                // Отбрасываем знаки препинания и заново считаем
+                symbols.ExceptWith(signs);
+                Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+            }
+            // Console.ReadKey();
 
         }
     }
