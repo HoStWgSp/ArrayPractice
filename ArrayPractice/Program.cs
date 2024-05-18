727276831a58c73a5262eaf46e1a36d85696e152
HoStWgSp <hostwg@vk.com>
2024-05-18 15:47:15 +1100

    Задания 13.4.4

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -6,42 +6,55 @@ using static ArrayPractice.Program;
 namespace ArrayPractice
 {
     internal class Program
-    {
+    {//  Объявим словарь с двумя значениями
+        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        {
+            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
+            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
+        };
         static void Main(string[] args)
         {
-            while (true)
+            // Покажем весь список
+            Console.WriteLine("Текущий список контактов: ");
+            WriteAllContacts();
+
+            // Попробуем добавить новый контакт, если такого ещё нет
+            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
+
+            //  Выведем обновлённый список
+            Console.WriteLine("Обновленный список контактов: ");
+            WriteAllContacts();
+
+            //  Попробуем достать контакт для изменения данных
+            if (PhoneBook.TryGetValue("Диана", out Contact contact))
+                contact.PhoneNumber = 79990000001;
+
+            // И покажем результат после изменения
+            Console.WriteLine("Список после изменения: ");
+            WriteAllContacts();
+            Console.ReadKey();
+
+        }
+        // Метод для вывода словаря на консоль
+        public static void WriteAllContacts()
+        {
+            foreach (var contact in PhoneBook)
+                Console.WriteLine(contact.Key + ": " + contact.Value.PhoneNumber);
+            Console.WriteLine();
+        }
+
+        // Класс был изменен. Поле Name мы убрали,
+        // так как оно теперь будет ключом
+        // для поиска значений в словаре
+        public class Contact // модель класса
+        {
+            public Contact(long phoneNumber, String email) // метод-конструктор
             {
-                Console.WriteLine("Введите строку");
-                // Сохраняем предложение в строку
-                var sentence = Console.ReadLine();
-
-                // сохраняем в массив char
-                var characters = sentence.ToCharArray();
-
-                var symbols = new HashSet<char>();
-
-                // добавляем во множество. Сохраняются только неповторяющиеся символы
-                foreach (var symbol in characters)
-                    symbols.Add(symbol);
-
-                // Выводим результат
-                Console.WriteLine(symbols.Count);
-                // сохраняем знаки препинания в массив Char
-                var signs = new[] { ',', ' ', '.' };
-
-                // сохраняем числовые символы в массив Char
-                var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-                //  Проверяем, есть ли цифры
-                bool containsNumbers = symbols.Overlaps(numbers);
-                Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
-
-                // Отбрасываем знаки препинания и заново считаем
-                symbols.ExceptWith(signs);
-                Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+                PhoneNumber = phoneNumber;
+                Email = email;
             }
-            // Console.ReadKey();
-
+            public long PhoneNumber { get; set; }
+            public String Email { get; set; }
         }
     }
 }
