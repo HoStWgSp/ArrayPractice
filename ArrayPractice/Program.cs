95d65bfdc5d954311f2a33fdfc26ffba8b83db72
HoStWgSp <hostwg@vk.com>
2024-05-18 16:10:34 +1100

    Задание 13.5.4

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -1,60 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using static ArrayPractice.Program;
 
 namespace ArrayPractice
 {
     internal class Program
-    {//  Объявим словарь с двумя значениями
-        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
-        {
-            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
-            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
-        };
+    {
+        public static Stack<string> words = new Stack<string>();
+
         static void Main(string[] args)
         {
-            // Покажем весь список
-            Console.WriteLine("Текущий список контактов: ");
-            WriteAllContacts();
-
-            // Попробуем добавить новый контакт, если такого ещё нет
-            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
-
-            //  Выведем обновлённый список
-            Console.WriteLine("Обновленный список контактов: ");
-            WriteAllContacts();
-
-            //  Попробуем достать контакт для изменения данных
-            if (PhoneBook.TryGetValue("Диана", out Contact contact))
-                contact.PhoneNumber = 79990000001;
-
-            // И покажем результат после изменения
-            Console.WriteLine("Список после изменения: ");
-            WriteAllContacts();
-            Console.ReadKey();
-
-        }
-        // Метод для вывода словаря на консоль
-        public static void WriteAllContacts()
-        {
-            foreach (var contact in PhoneBook)
-                Console.WriteLine(contact.Key + ": " + contact.Value.PhoneNumber);
+            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в стек.");
             Console.WriteLine();
-        }
 
-        // Класс был изменен. Поле Name мы убрали,
-        // так как оно теперь будет ключом
-        // для поиска значений в словаре
-        public class Contact // модель класса
-        {
-            public Contact(long phoneNumber, String email) // метод-конструктор
+            while (true)
             {
-                PhoneNumber = phoneNumber;
-                Email = email;
+                var input = Console.ReadLine();
+
+                words.Push(input); // Изменить здесь
+
+
+                Console.WriteLine();
+                Console.WriteLine("В стеке:");
+
+                foreach (var word in words)
+                {
+                    Console.WriteLine(" " + word);
+                }
             }
-            public long PhoneNumber { get; set; }
-            public String Email { get; set; }
+
+            Console.ReadKey();
         }
+        
     }
 }
