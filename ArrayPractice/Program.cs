d313e23c99277ff105513ffcea507ac7572e544f
HoStWgSp <hostwg@vk.com>
2024-05-18 16:23:02 +1100

    Задание 13.5.4

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -17,11 +17,29 @@ namespace ArrayPractice
 
             while (true)
             {
-                var input = Console.ReadLine();
-
-                words.Push(input); // Изменить здесь
+                string input = Console.ReadLine();
 
 
+                
+
+                switch (input)
+                {
+                    //   если  команда pop - пробуем достать элемент
+                    case "pop":
+                        words.TryPop(out string popResult);
+                        Console.WriteLine("Удалено: {0}", popResult);
+                        break;
+                    //   если  команда peek - пробуем  просмотреть элемент
+                    case "peek":
+                        words.TryPeek(out string peekResult);
+                        Console.WriteLine("rgfewgdfgdgd {0}",peekResult);
+                        break;
+                    default:
+                        // если ни одна из команд не распознана - простов стек добавляем то что введено
+                        if (string.IsNullOrEmpty(input)) break;
+                        break;
+                }
+                words.Push(input);
                 Console.WriteLine();
                 Console.WriteLine("В стеке:");
 
